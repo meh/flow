80ba3404b903faeb1e5832867ec7cf22127e4c55
meh <meh@schizofreni.co>
2012-11-13 17:25:49 +0100

    database: move find_flow export in the order it's defined

--- src/flow_database.erl
+++ src/flow_database.erl
@@ -22,7 +22,7 @@
 -export([install/1, uninstall/0]).
 -export([create_float/1, create_float/2, find_float/1, find_or_create_float/1]).
 -export([create_drop/1, create_drop/2]).
--export([create_flow/3, find_flow/1, add_floats/2, delete_floats/2, find_flows/1]).
+-export([create_flow/3, add_floats/2, delete_floats/2, find_flow/1, find_flows/1]).
 -export([create_moderator/1, delete_moderator/1, is_moderator/1]).
 
 install(Nodes) ->
